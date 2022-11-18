7986815dab60d1e2e6d89fe11c109bc37685c82c
Angie_Mejia <mejiagonzaleza881@gmail.com>
2022-11-18 12:41:42 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <h2>hola como estas</h2>
+    </div>);
+}
+
+export default App;
